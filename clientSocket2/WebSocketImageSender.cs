0f1f0c4483d690fd7abf7cf81b99562a64c32c8c
Hao Qin <u0169876@kuleuven.be>
2024-06-20 09:27:38 +0200

    init

---
+++ clientSocket2/WebSocketImageSender.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using Newtonsoft.Json;
+//using System.Text.Json;
+
+
+namespace clientSocket
+{
+    public class WebSocketImageSender
+    {
+        private WebSocket _webSocket;
+        private string _resultsFolder;
+        private string _imgPath;
+
+        public WebSocketImageSender(string serverUri, string resultsFolder)
+        {
+            _resultsFolder = resultsFolder;
+            //_imgPath = imgPath;
+            _webSocket = new WebSocket(serverUri);
+            _webSocket.OnMessage += OnMessage;
+            _webSocket.OnOpen += OnOpen;
+            _webSocket.OnClose += Onclose;
+            _webSocket.OnError += OnError;
+
+        }
+        public void Connect()
+        { 
+            _webSocket.Connect();
+        }
+
+        public void Disconnect()
+        { _webSocket.Close(); }
+
+        public void sendImage(byte[] imageBytes, string imageName, string imgPath)
+        {
+            _imgPath = imgPath;
+            var metadata = new
+            {
+                image_name = imageName
+            };
+
+            if (!_webSocket.IsAlive)
+            {
+                try {
+                    _webSocket.Connect();
+                }
+                catch
+                {
+                    throw new InvalidCastException("WebSocket connection is not open.");
+                }   
+            }
+
+            string metadataJson = JsonConvert.SerializeObject(metadata);
+            //string metadataJson = JsonSerializer.Serialize(metadata);
+
+            _webSocket.Send(metadataJson);
+            _webSocket.Send(imageBytes);
+            //await Task.Run(() => _webSocket.Send(imageBytes));
+
+        }
+
+        private void SaveResults(string imagePath, MessageEventArgs e, string resultsFolder)
+        {
+            string resultPath = Path.Combine(resultsFolder, Path.GetFileNameWithoutExtension(imagePath) + ".json");
+            File.WriteAllText(resultPath, e.Data);
+        }
+
+
+
+        private void OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.IsText)
+            {   
+                //var response = JsonConvert.DeserializeObject(e.Data);
+                //File.WriteAllText(resultsFolder, e.Data);
+                SaveResults(_imgPath, e, _resultsFolder);
+                Console.WriteLine("Prediction Get");
+            }
+            
+        }
+
+        private void Onclose(object sender, CloseEventArgs e)
+        {
+            Console.WriteLine("Websocket connection closed", e.Reason);
+        }
+
+        private void OnOpen(object sender, EventArgs e)
+        {
+            Console.WriteLine("Websocket connection opened.");
+        }
+
+        private void OnError(object sender, WebSocketSharp.ErrorEventArgs e)
+        {
+            Console.WriteLine("WebSocket error: " + e.Message);
+        }
+
+
+    }
+}
---
+++ watchdog/Watchdog.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+//using client;
+//using TimingLoggerLib;
+using Serilog;
+using System.Configuration;
+//using System.Collections.Specialized;
+using clientSocket;
+//using System.Reflection;
+
+
+namespace Watchdog
+{
+    class Watchdog
+    {
+        static void Main()
+        {
+            string logPath = ConfigurationManager.AppSettings.Get("logPath")!;
+            string shareFolder = ConfigurationManager.AppSettings.Get("shareFolder")!;
+            string resultsFolder = ConfigurationManager.AppSettings.Get("resultsFolder")!;
+            string serverUrl = ConfigurationManager.AppSettings.Get("serverUrl")!;
+
+            WebSocketImageSender wbSender = new WebSocketImageSender(serverUrl, resultsFolder);
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(logPath)
+                .CreateLogger();
+
+            Log.Information("Start watchdog");
+            try
+            {
+                wbSender.Connect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            using var watcher = new FileSystemWatcher(shareFolder);
+
+            watcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+            //
+            //watcher.Changed += OnChanged;
+            watcher.Created += (sender, e) => OnCreated(sender, e, resultsFolder, serverUrl, wbSender);
+            watcher.Deleted += OnDeleted;
+            watcher.Error += OnError;
+
+            watcher.Filter = "*.*";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            Console.WriteLine($"Changed: {e.FullPath}");
+        }
+
+        private static async void OnCreated(object sender, FileSystemEventArgs e, 
+            string resultsFolder, string serverUrl, WebSocketImageSender wbSender)
+        {
+            // var watch = System.Diagnostics.Stopwatch.StartNew();
+            //var timingLogger = new TimingLogger();
+
+            string file_path = e.FullPath;
+            //Console.WriteLine(file_path);
+            Log.Information(file_path);
+            string fileExtension = Path.GetExtension(e.FullPath).ToLower();
+            if (fileExtension == ".jpg" || fileExtension == ".png" || fileExtension == ".jpeg" || fileExtension == ".bmp")
+            {
+                try{
+                    //timingLogger.LogTimingStart(1, "Post");
+                    //var client = new Client(resultsFolder);
+                    Thread.Sleep(1000);
+                    string imageName = Path.GetFileName(file_path);
+                    byte[] imageData = File.ReadAllBytes(file_path);
+                    wbSender.sendImage(imageData, imageName, file_path);
+
+                    // Console.WriteLine("Elapsed time: " + elapsedMs + "ms");
+                }
+                catch (Exception ex) { 
+                    Log.Error(ex.ToString());
+                    //Console.WriteLine("Error uploading image: " + ex.Message);
+                }
+                finally
+                {
+                    await Log.CloseAndFlushAsync(); // ensure all logs written before app exits
+                }
+            }
+                     
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e) =>
+            Console.WriteLine($"Deleted: {e.FullPath}");
+
+        private static void OnError(object sender, ErrorEventArgs e) =>
+            PrintException(e.GetException());
+
+        private static void PrintException(Exception? ex)
+        {
+            if (ex != null)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine("Stacktrace:");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine();
+                PrintException(ex.InnerException);
+            }
+        }
+    }
+}
